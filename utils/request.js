cfb3b50de81479d4117ba88e0ffa2436dbfb51b3
liushaochuan <liushaochuan@boyue.com>
2018-07-25 17:40:12 +0800

    第一次提交

---
+++ utils/request.js
@@ -0,0 +1,13 @@
+export default request = () => {
+  wx.request({
+    url: '',
+    data: '',
+    header: {},
+    method: 'GET',
+    dataType: 'json',
+    responseType: 'text',
+    success: function (res) {},
+    fail: function (res) {},
+    complete: function (res) {},
+  })
+}
